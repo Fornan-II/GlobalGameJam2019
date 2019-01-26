fc7799fcc2b228245ff751b800566578ea1fefd1
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-01-26 00:18:32 -0500

    WorldShader WIP
    Functionality, needs outline support

---
+++ Assets/Scripts/UpdateWorldShader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateWorldShader : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Shader.SetGlobalVector("_Position", transform.position);
+    }
+}
